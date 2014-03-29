cfed795fdf0a0bca998203cf2d579016328d8ee8
Olivier R-D <olivier@dummy.com>
2014-03-29 09:21:11 +0100

    use 2 spaces tabs in node.* as in rest of opcua project

--- include/opc/common/node.h
+++ include/opc/common/node.h
@@ -1,20 +1,20 @@
 /******************************************************************************
- *   Copyright (C) 2014-2014 Olivier Roulet-Dubonnet                  *
- *   olivier.roulet@gmail.com                  *
- *                                          *
- *   This library is free software; you can redistribute it and/or modify     *
- *   it under the terms of the GNU Lesser General Public License as          *
- *   published by the Free Software Foundation; version 3 of the License.     *
- *                                          *
- *   This library is distributed in the hope that it will be useful,          *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *   GNU Lesser General Public License for more details.              *
- *                                          *
+ *   Copyright (C) 2014-2014 Olivier Roulet-Dubonnet          *
+ *   olivier.roulet@gmail.com          *
+ *                      *
+ *   This library is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as      *
+ *   published by the Free Software Foundation; version 3 of the License.   *
+ *                      *
+ *   This library is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+ *   GNU Lesser General Public License for more details.        *
+ *                      *
  *   You should have received a copy of the GNU Lesser General Public License *
- *   along with this library; if not, write to the                  *
- *   Free Software Foundation, Inc.,                          *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              *
+ *   along with this library; if not, write to the          *
+ *   Free Software Foundation, Inc.,              *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.        *
  ******************************************************************************/
 
 #ifndef NODE_H
@@ -28,57 +28,55 @@
 
 namespace OpcUa
 {
-    class Node
-    {
-        public:
-            Node( const std::shared_ptr<OpcUa::Remote::Computer> server, const NodeID nodeid ) {
-                this->server = server;
-                this->NodeId = nodeid;
-                mIsNull = false;
-            }
-            Node( const std::shared_ptr<OpcUa::Remote::Computer> server){ this->server = server; mIsNull = true; };
-            //~Node(){};
-            bool isNull(){ return mIsNull; }
-            NodeID GetNodeId() {return NodeId;}
+  class Node
+  {
+    public:
+      Node( const std::shared_ptr<OpcUa::Remote::Computer> server, const NodeID nodeid ) {
+        this->server = server;
+        this->NodeId = nodeid;
+        mIsNull = false;
+      }
+      Node( const std::shared_ptr<OpcUa::Remote::Computer> server){ this->server = server; mIsNull = true; };
+      //~Node(){};
+      bool isNull(){ return mIsNull; }
+      NodeID GetNodeId() {return NodeId;}
 
-            //The tree common Opc-Ua methods
-            std::vector<Node> Browse( OpcUa::ReferenceID refid=OpcUa::ReferenceID::HierarchicalReferences);
-            Variant Read(OpcUa::AttributeID const attr);
-            //std::vector<DataValue> ReadVector(OpcUa::AttributeID const attr);;
-            std::vector<StatusCode> Write(OpcUa::AttributeID const attr, Variant val);
-            //void Write(OpcUa::AttributeID const attr, const std::vector<Variant>);
+      //The tree common Opc-Ua methods
+      std::vector<Node> Browse( OpcUa::ReferenceID refid=OpcUa::ReferenceID::HierarchicalReferences);
+      Variant Read(OpcUa::AttributeID const attr);
+      //std::vector<DataValue> ReadVector(OpcUa::AttributeID const attr);;
+      std::vector<StatusCode> Write(OpcUa::AttributeID const attr, Variant val);
 
-            //Helper methods
-            void WriteValue(Variant variant);
-            Variant ReadValue();
-            Variant ReadDataType();
-            //std::vector<DataValue> ReadValueVector();
-            void SetBrowseNameCache(QualifiedName browsename){this->browseName= browsename;}  
-            void WriteBrowseName(QualifiedName browsename); 
-            QualifiedName GetBrowseName() { return browseName; }
-            Node FindChildNode(std::string browsename, ushort ns=0);
-            Node FindChildNode(QualifiedName browsename);
+      //Helper methods
+      void WriteValue(Variant variant);
+      Variant ReadValue();
+      Variant ReadDataType();
+      //std::vector<DataValue> ReadValueVector();
+      void SetBrowseNameCache(QualifiedName browsename){this->browseName= browsename;}  
+      void WriteBrowseName(QualifiedName browsename); 
+      QualifiedName GetBrowseName() { return browseName; }
+      std::vector<Node> GetProperties() {return Browse(OpcUa::ReferenceID::HasProperty);}
+      std::vector<Node> GetChildren() {return Browse();}
+      std::vector<Node> GetVariables() {return Browse(OpcUa::ReferenceID::HasComponent);} //Not correct should filter by variable type
 
-            //void WritePropertyChildValue(QualifiedName browsename, Variant val);
-            //void WritePropertyChildValue(ushort ns, std::string browsename, Variant val);
-            //Variant ReadPropertyChildValue (std::string browsename, ushort ns=0);
-            //Variant ReadPropertyChildValue (QualifiedName browsename);
+      Node FindChildNode(std::string browsename, ushort ns=0);
+      Node FindChildNode(QualifiedName browsename);
 
-            std::string ToString() const; 
-            explicit operator bool() const {return !mIsNull;}
+      std::string ToString() const; 
+      explicit operator bool() const {return !mIsNull;}
 
 
-        private:
-            std::shared_ptr<OpcUa::Remote::Computer> server;
-            bool mIsNull = true;
-            NodeID NodeId;
-            QualifiedName browseName ;
-            friend std::ostream& operator<<(std::ostream& os, const Node& node){
-                os << node.ToString();
-                return os;
-            }
+    private:
+      std::shared_ptr<OpcUa::Remote::Computer> server;
+      bool mIsNull = true;
+      NodeID NodeId;
+      QualifiedName browseName ;
+      friend std::ostream& operator<<(std::ostream& os, const Node& node){
+        os << node.ToString();
+        return os;
+      }
 
-    };
+  };
 } // namespace OpcUa
 
 #endif
--- src/common/node.cpp
+++ src/common/node.cpp
@@ -1,20 +1,20 @@
 /******************************************************************************
- *   Copyright (C) 2014-2014 Olivier Roulet-Dubonnet                  *
- *   olivier.roulet@gmail.com                  *
- *                                          *
- *   This library is free software; you can redistribute it and/or modify     *
- *   it under the terms of the GNU Lesser General Public License as          *
- *   published by the Free Software Foundation; version 3 of the License.     *
- *                                          *
- *   This library is distributed in the hope that it will be useful,          *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *   GNU Lesser General Public License for more details.              *
- *                                          *
+ *   Copyright (C) 2014-2014 Olivier Roulet-Dubonnet          *
+ *   olivier.roulet@gmail.com          *
+ *                      *
+ *   This library is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as      *
+ *   published by the Free Software Foundation; version 3 of the License.   *
+ *                      *
+ *   This library is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+ *   GNU Lesser General Public License for more details.        *
+ *                      *
  *   You should have received a copy of the GNU Lesser General Public License *
- *   along with this library; if not, write to the                  *
- *   Free Software Foundation, Inc.,                          *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              *
+ *   along with this library; if not, write to the          *
+ *   Free Software Foundation, Inc.,              *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.        *
  ******************************************************************************/
 
 
@@ -24,146 +24,146 @@
 
 namespace OpcUa
 {
-        Variant Node::Read(OpcUa::AttributeID attr)
-        {
-            ReadParameters params;
-            AttributeValueID attribute;
-            attribute.Node = this->NodeId;
-            attribute.Attribute = attr;
-            params.AttributesToRead.push_back(attribute);
-            DataValue dv =  server->Attributes()->Read(params).front();
-            return dv.Value;
-        }
+  Variant Node::Read(OpcUa::AttributeID attr)
+  {
+    ReadParameters params;
+    AttributeValueID attribute;
+    attribute.Node = this->NodeId;
+    attribute.Attribute = attr;
+    params.AttributesToRead.push_back(attribute);
+    DataValue dv =  server->Attributes()->Read(params).front();
+    return dv.Value;
+  }
 
-        Variant Node::ReadValue()
-        {
-            return Read(OpcUa::AttributeID::VALUE);
-        }
+  Variant Node::ReadValue()
+  {
+    return Read(OpcUa::AttributeID::VALUE);
+  }
 
-        Variant Node::ReadDataType()
-        {
-            return Read(OpcUa::AttributeID::DATA_TYPE);
-        }
+  Variant Node::ReadDataType()
+  {
+    return Read(OpcUa::AttributeID::DATA_TYPE);
+  }
 
-        std::vector<StatusCode> Node::Write(OpcUa::AttributeID const attr, Variant value)
-        {
-            OpcUa::WriteValue attribute;
-            attribute.Node = NodeId;
-            attribute.Attribute = attr;
-            attribute.Data = value;
-            return server->Attributes()->Write(std::vector<OpcUa::WriteValue>(1, attribute));
-        }
-
-
-        std::vector<Node> Node::Browse(OpcUa::ReferenceID refid)
-        {
-            OpcUa::BrowseDescription description;
-            description.NodeToBrowse = this->NodeId;
-            description.Direction = OpcUa::BrowseDirection::Forward;
-            description.IncludeSubtypes = true;
-            description.NodeClasses = OpcUa::NODE_CLASS_ALL;
-            description.ResultMask = OpcUa::REFERENCE_ALL;
-            description.ReferenceTypeID =  refid;
-
-            OpcUa::NodesQuery query;
-            query.NodesToBrowse.push_back(description);
-            query.MaxReferenciesPerNode = 100;
-            std::vector<Node> nodes;
-            std::vector<OpcUa::ReferenceDescription> refs = server->Views()->Browse(query);
-            while(true)
-            {
-              if (refs.empty())
-              {
-                break;
-              }
-              for (auto refIt : refs)
-              {
-                  Node node(server, refIt.TargetNodeID);
-                  std::cout << "Creating node with borwsename: " << refIt.BrowseName.NamespaceIndex << refIt.BrowseName.Name << std::endl;
-                  node.SetBrowseNameCache(refIt.BrowseName);
-                  nodes.push_back(node);
-              }
-              refs = server->Views()->BrowseNext();
-            }
-            return nodes;
-        }
+  std::vector<StatusCode> Node::Write(OpcUa::AttributeID const attr, Variant value)
+  {
+    OpcUa::WriteValue attribute;
+    attribute.Node = NodeId;
+    attribute.Attribute = attr;
+    attribute.Data = value;
+    return server->Attributes()->Write(std::vector<OpcUa::WriteValue>(1, attribute));
+  }
 
 
-        Node Node::FindChildNode(std::string browsename, ushort ns)
-        {
-            QualifiedName qn(ns, browsename);
-            return FindChildNode(qn);
-        }
+  std::vector<Node> Node::Browse(OpcUa::ReferenceID refid)
+  {
+    OpcUa::BrowseDescription description;
+    description.NodeToBrowse = this->NodeId;
+    description.Direction = OpcUa::BrowseDirection::Forward;
+    description.IncludeSubtypes = true;
+    description.NodeClasses = OpcUa::NODE_CLASS_ALL;
+    description.ResultMask = OpcUa::REFERENCE_ALL;
+    description.ReferenceTypeID =  refid;
 
-        Node Node::FindChildNode(QualifiedName browsename)
-        {
-            //FIXME: Is there a more efficient method?
-            for (Node node : Browse())
-            {
-                std::cout << "Comparing: " <<node.GetBrowseName().Name << " to " << browsename.Name << std::endl;
-                if (node.GetBrowseName() == browsename)
-                {
-                    return node;
-                }
-            }
-            return Node(server); //Null node
-        }
+    OpcUa::NodesQuery query;
+    query.NodesToBrowse.push_back(description);
+    query.MaxReferenciesPerNode = 100;
+    std::vector<Node> nodes;
+    std::vector<OpcUa::ReferenceDescription> refs = server->Views()->Browse(query);
+    while(true)
+    {
+      if (refs.empty())
+      {
+      break;
+      }
+      for (auto refIt : refs)
+      {
+        Node node(server, refIt.TargetNodeID);
+        std::cout << "Creating node with borwsename: " << refIt.BrowseName.NamespaceIndex << refIt.BrowseName.Name << std::endl;
+        node.SetBrowseNameCache(refIt.BrowseName);
+        nodes.push_back(node);
+      }
+      refs = server->Views()->BrowseNext();
+    }
+    return nodes;
+  }
 
-        std::string Node::ToString() const
-        {
-            std::ostringstream os;
-            os << "Node(" << browseName.NamespaceIndex <<":"<< browseName.Name << ", id=" ;
-            OpcUa::NodeIDEncoding encoding = static_cast<OpcUa::NodeIDEncoding>(NodeId.Encoding & OpcUa::NodeIDEncoding::EV_VALUE_MASK);
 
-            switch (encoding)
-            {
-              case OpcUa::NodeIDEncoding::EV_TWO_BYTE:
-              {
-                os << (unsigned)NodeId.TwoByteData.Identifier ;
-                break;
-              }
+  Node Node::FindChildNode(std::string browsename, ushort ns)
+  {
+    QualifiedName qn(ns, browsename);
+    return FindChildNode(qn);
+  }
 
-              case OpcUa::NodeIDEncoding::EV_FOUR_BYTE:
-              {
-                os << (unsigned)NodeId.FourByteData.NamespaceIndex << ":" << (unsigned)NodeId.FourByteData.Identifier ;
-                break;
-              }
+  Node Node::FindChildNode(QualifiedName browsename)
+  {
+    //FIXME: Should be implemented at lower level using filter browsing and breaking browsing
+    for (Node node : Browse())
+    {
+      std::cout << "Comparing: " <<node.GetBrowseName().Name << " to " << browsename.Name << std::endl;
+      if (node.GetBrowseName() == browsename)
+      {
+        return node;
+      }
+    }
+    return Node(server); //Null node
+  }
 
-              case OpcUa::NodeIDEncoding::EV_NUMERIC:
-              {
-                os << (unsigned)NodeId.NumericData.NamespaceIndex << ":" << (unsigned)NodeId.NumericData.Identifier ;
-                break;
-              }
+  std::string Node::ToString() const
+  {
+    std::ostringstream os;
+    os << "Node(" << browseName.NamespaceIndex <<":"<< browseName.Name << ", id=" ;
+    OpcUa::NodeIDEncoding encoding = static_cast<OpcUa::NodeIDEncoding>(NodeId.Encoding & OpcUa::NodeIDEncoding::EV_VALUE_MASK);
 
-              case OpcUa::NodeIDEncoding::EV_STRING:
-              {
-                os << (unsigned)NodeId.StringData.NamespaceIndex << ":" << NodeId.StringData.Identifier << std::endl;
-                break;
-              }
+    switch (encoding)
+    {
+      case OpcUa::NodeIDEncoding::EV_TWO_BYTE:
+      {
+        os << (unsigned)NodeId.TwoByteData.Identifier ;
+        break;
+      }
 
-              case OpcUa::NodeIDEncoding::EV_BYTE_STRING:
-              {
-                os << (unsigned)NodeId.BinaryData.NamespaceIndex ; //<< for (auto val : NodeId.BinaryData.Identifier) {std::cout << (unsigned)val; }
-                //std::cout << std::endl;
-                break;
-              }
+      case OpcUa::NodeIDEncoding::EV_FOUR_BYTE:
+      {
+        os << (unsigned)NodeId.FourByteData.NamespaceIndex << ":" << (unsigned)NodeId.FourByteData.Identifier ;
+        break;
+      }
 
-              case OpcUa::NodeIDEncoding::EV_GUID:
-              {
-                os << "Guid: " << (unsigned)NodeId.GuidData.NamespaceIndex ;
-                const OpcUa::Guid& guid = NodeId.GuidData.Identifier;
-                os << ":" << std::hex << guid.Data1 << "-" << guid.Data2 << "-" << guid.Data3;
-                for (auto val : guid.Data4) {os << (unsigned)val; }
-                break;
-              }
-              default:
-              {
-                os << "unknown id type:" << (unsigned)encoding ;
-                break;
-              }
-            }
-            os << ")";
-            return os.str();
-        }
+      case OpcUa::NodeIDEncoding::EV_NUMERIC:
+      {
+        os << (unsigned)NodeId.NumericData.NamespaceIndex << ":" << (unsigned)NodeId.NumericData.Identifier ;
+        break;
+      }
+
+      case OpcUa::NodeIDEncoding::EV_STRING:
+      {
+        os << (unsigned)NodeId.StringData.NamespaceIndex << ":" << NodeId.StringData.Identifier << std::endl;
+        break;
+      }
+
+      case OpcUa::NodeIDEncoding::EV_BYTE_STRING:
+      {
+        os << (unsigned)NodeId.BinaryData.NamespaceIndex ; //<< for (auto val : NodeId.BinaryData.Identifier) {std::cout << (unsigned)val; }
+        //std::cout << std::endl;
+        break;
+      }
+
+      case OpcUa::NodeIDEncoding::EV_GUID:
+      {
+        os << "Guid: " << (unsigned)NodeId.GuidData.NamespaceIndex ;
+        const OpcUa::Guid& guid = NodeId.GuidData.Identifier;
+        os << ":" << std::hex << guid.Data1 << "-" << guid.Data2 << "-" << guid.Data3;
+        for (auto val : guid.Data4) {os << (unsigned)val; }
+        break;
+      }
+      default:
+      {
+        os << "unknown id type:" << (unsigned)encoding ;
+        break;
+      }
+    }
+    os << ")";
+    return os.str();
+  }
 
 }
